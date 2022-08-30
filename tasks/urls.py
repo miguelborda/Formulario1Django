425dbc5b56945b7d0055a6552894361dfa5c8ad1
miguelborda <miguelangelbg20@gmail.com>
2022-08-30 00:18:27 -0400

    Formulario simple con Django y BD Postgresql

---
+++ tasks/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import create_task, list_tasks, delete_task
+
+urlpatterns = [
+    path('', list_tasks),
+    path('new_task/',create_task, name='create_task'),
+    path('delete_task/<int:task_id>/', delete_task, name='delete_task'),
+]
